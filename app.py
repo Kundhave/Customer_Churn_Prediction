27c97f34ba2500d0739f2bfd96572b04853c6c00
Kundhave <kundhave05@gmail.com>
2025-03-23 14:35:18 +0530

    Initial Commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
 
-# Load models & scaler
 xgb_model = pickle.load(open("xgb_model.pkl", "rb"))
 rf_model = pickle.load(open("rf_model.pkl", "rb"))
 scaler = pickle.load(open("scaler.pkl", "rb"))
@@ -11,34 +10,28 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")  # Load frontend
+    return render_template("index.html") 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     try:
-        # Get input data from the request
         data = request.json
         features = np.array(data["features"]).reshape(1, -1)
-        model_choice = data.get("model", "xgb")  # Default to XGBoost
+        model_choice = data.get("model", "xgb") 
 
-        # Scale input
         features_scaled = scaler.transform(features)
-
-        # Select model
         model = rf_model if model_choice == "rf" else xgb_model
 
-        # Predict churn
-        prediction = model.predict(features_scaled)[0]  # 0 or 1
-        probability = model.predict_proba(features_scaled)[0][1]  # Probability of churn (class 1)
+        prediction = model.predict(features_scaled)[0]  
+        probability = model.predict_proba(features_scaled)[0][1]  
 
-        # Determine risk level
         risk_level = "Low Risk" if prediction == 0 else "High Risk"
-        probability_percent = round(probability * 100, 2)  # Convert to percentage
+        probability_percent = round(probability * 100, 2) 
 
-        # Prepare response
+        
         response = {
             "selected_model": model_choice,
-            "risk_level": risk_level,  # Only include descriptive risk level
+            "risk_level": risk_level,  
             "churn_probability": f"{probability_percent}%"
         }
 
